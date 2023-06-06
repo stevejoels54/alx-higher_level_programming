3c72a8f4a5c3e1cddfa3b2958af7cdff1ce5ea41
joelsteven <joelofelectronics@gmail.com>
2023-06-06 04:10:17 +0300

    Added newline to info

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-info = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+info = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(info)
 sys.exit(1)
