7c4ff672e12c9e012197ba97aefb40c7fc83201e
joelsteven <joelofelectronics@gmail.com>
2023-06-07 02:51:01 +0300

    Fixed negative powers

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
     product = 1
-    for num in range(b):
-        product = product * a
+    if b >= 0:
+        for num in range(b):
+            product = product * a
+    else:
+        return 1 / pow(a, abs(b))
     return product
