61a864a34bdb86d5b3755e2a778ee920fdcc0292
joelsteven <joelofelectronics@gmail.com>
2023-06-09 00:56:40 +0300

    Fixed 4 hidden discovery task

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def get_names():
+    names = dir(hidden_4)
+    return sorted(name for name in names if not name.startswith('__'))
+
+
+if __name__ == "__main__":
+    names = get_names()
+    for name in names:
+        print(name)
