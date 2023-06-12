9569ebdd10d65f555dd8836bde152d746a253bc9
joelsteven <joelofelectronics@gmail.com>
2023-06-13 00:24:56 +0300

    Fixed the row iterator

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in range matrix:
+    for row in matrix:
         for col in range(len(row)):
             if col != len(row) - 1:
                 print("{:d} ".format(row[col]), end="")
