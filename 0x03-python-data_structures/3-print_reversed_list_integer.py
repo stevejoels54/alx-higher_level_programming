60d36f11e053e45b895bddd24147861a2eb92f65
joelsteven <joelofelectronics@gmail.com>
2023-06-12 21:43:55 +0300

    Fixed 3 task by checking None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for val in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[val]))
+    if my_list is not None:
+        for val in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[val]))
