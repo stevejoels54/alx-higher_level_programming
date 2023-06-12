e1c68add436d70dfe612d274d558496c576005d4
joelsteven <joelofelectronics@gmail.com>
2023-06-13 02:41:41 +0300

    Fixed task 11 by using del function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,9 +3,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])
+    delete_list = my_list.copy()
+    del delete_list[idx]
 
     return new_list
