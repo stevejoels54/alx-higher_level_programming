f09b3d30e5493dbe10eb50c6084ff6ac91a16a26
joelsteven <joelofelectronics@gmail.com>
2023-06-13 02:55:39 +0300

    Fixed task 11 to return the same array as deleted

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    delete_list = my_list.copy()
-    del delete_list[idx]
+    del my_list[idx]
 
-    return delete_list
+    return my_list
