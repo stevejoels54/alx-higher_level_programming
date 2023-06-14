8e01a09ebe32e3366d1fa1d4ca3a6683993be2b8
joelsteven <joelofelectronics@gmail.com>
2023-06-14 17:40:21 +0300

    Removed tests

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -7,14 +7,3 @@ def square_matrix_simple(matrix=[]):
             new_row.append(num * num)
         new_matrix.append(new_row)
     return new_matrix
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
