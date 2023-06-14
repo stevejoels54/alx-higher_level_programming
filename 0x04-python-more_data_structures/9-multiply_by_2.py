87718cbe418706ad157be15e62d20cc4515f1727
joelsteven <joelofelectronics@gmail.com>
2023-06-14 22:31:55 +0300

    Fixed task 9 by multiplying by 2 not square

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for key in a_dictionary:
-        new_dict[key] = a_dictionary[key] * a_dictionary[key]
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
