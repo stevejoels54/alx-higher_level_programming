5f3f8bee31fd50d711e12bd1d2ac56552f79983a
joelsteven <joelofelectronics@gmail.com>
2023-06-27 18:36:30 +0300

    Added task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """square class representation"""
+
+    def __init__(self, size):
+        """init new square
+        Args:
+            size (int): size of new square
+        """
+
+        self.__size = size
