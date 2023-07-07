6e92fb4c34bb207633ca6c2166668281c46ccd73
joelsteven <joelofelectronics@gmail.com>
2023-07-07 23:20:59 +0300

    Added task 5 with it's tests

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Function to print text with indentation"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each occurrence of '.', '?', and ':' characters.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    lines = []
+    current_line = ""
+    punctuation = [".", "?", ":"]
+    
+
+    for char in text:
+        current_line += char
+        if char in punctuation:
+            lines.append(current_line.strip())
+            current_line = ""
+
+    if current_line:
+        lines.append(current_line.strip())
+
+    for line in lines:
+        print(line)
