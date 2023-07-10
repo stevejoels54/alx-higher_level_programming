5955a47b3b8f414ba1a92c45a15a97f17febc1af
joelsteven <joelofelectronics@gmail.com>
2023-07-10 23:47:55 +0300

    Added task 4 with tests

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class that inherited (directly or indirectly) from specified class"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited
+   (directly or indirectly) from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The specified class to compare against.
+
+    Returns:
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class;
+        False otherwise.
+    """
+    return (
+        isinstance(obj, type) and
+        issubclass(type(obj), a_class) and
+        type(obj) != a_class
+    )
