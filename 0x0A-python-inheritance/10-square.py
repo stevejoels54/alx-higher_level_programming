818a6e0c8c07c50e2ff1934aead788fb26c06bd3
joelsteven <joelofelectronics@gmail.com>
2023-07-11 00:58:58 +0300

    Added task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines class square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class, inherits from Rectangle.
+
+    Methods:
+        __init__(self, size): Initialize a Square instance.
+        area(self): Calculate and return the area of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = 0
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+        """
+        return self.__size ** 2
