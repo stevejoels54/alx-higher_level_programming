4bfb6b3596a66726edc6d6173dc5a46bc09ed8b1
joelsteven <joelofelectronics@gmail.com>
2023-07-12 01:08:07 +0300

    Fixed task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,13 +3,15 @@
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def load_from_json_file(filename):
     """
-    Write an object to a text file using a JSON representation
+    Create an object from a JSON file
 
     Args:
-        my_obj: The object to be written
-        filename: The name of the file to write to
+        filename: Name of the JSON file
+
+    Returns:
+        Object created from the JSON file
     """
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+    with open(filename, 'r') as file:
+        return json.load(file)
