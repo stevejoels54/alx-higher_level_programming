03a943b0185874add46b357335e0cc5063258222
joelsteven <joelofelectronics@gmail.com>
2023-07-18 02:24:52 +0300

    Added last base mandatory tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,3 +37,75 @@ class Base:
         if list_dictionaries is None or not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs (list): List of instances.
+
+        Returns:
+            None
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list of the JSON string representation json_string.
+
+        Args:
+            json_string (str): JSON string representation of a list of
+                dictionaries.
+
+        Returns:
+            list: If json_string is None or empty - returns an empty list.
+                Otherwise, return the list represented by json_string.
+        """
+        if json_string is None or not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return instance with all attributes already set.
+
+        Args:
+            **dictionary (dict): Key/value pairs of attributes to initialize.
+
+        Returns:
+            Instance of the class called.
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances.
+
+        Args:
+            None
+
+        Returns:
+            If the file doesn’t exist, return an empty list.
+            Otherwise, return a list of instances - the type of these
+            instances depends on cls (current class using this method).
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = cls.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
