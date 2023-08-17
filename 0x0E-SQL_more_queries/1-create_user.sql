f0e3be0c2d4ecc0f6bde1178289250e28cfaa21c
joelsteven <joelofelectronics@gmail.com>
2023-08-18 01:23:33 +0300

    minor fixes'

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Creates user user_0d_1 with all the privileges.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- Creates the database hbtn_0d_2 and the user user_0d_2 with SELECT privileges.                                                                      
 GRANT USAGE ON *.* TO `user_0d_2`@`localhost`                                                                                 
 GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`
+FLUSH PRIVILEGES;
