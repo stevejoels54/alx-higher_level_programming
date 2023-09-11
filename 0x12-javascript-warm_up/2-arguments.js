33dd537e04815a68054ee2df8357bb7df78a1065
joelsteven <joelofelectronics@gmail.com>
2023-09-12 02:01:09 +0300

    Fixed typo

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argLength = process.argv.length;
-console.log(argLength === 3 ? 'Argument found' : argLength >= 4 ? 'Arguments founds' : 'No argumrnt');
+console.log(argLength === 3 ? 'Argument found' : argLength >= 4 ? 'Arguments founds' : 'No argument');
