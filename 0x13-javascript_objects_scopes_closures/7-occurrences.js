98f99ca3127f5238716fae9d931db8f235a244ff
joelsteven <joelofelectronics@gmail.com>
2023-09-12 23:13:37 +0300

    Added task 7-occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+
+  for (let x = 0; x < list.length; x++) {
+    if (list[x] === searchElement) {
+      count++;
+    }
+  }
+
+  return count;
+};
