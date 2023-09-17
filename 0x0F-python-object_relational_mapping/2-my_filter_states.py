0b49d889f9b385ab31461c0c0109b2936acc4141
joelsteven <joelofelectronics@gmail.com>
2023-09-18 01:46:20 +0300

    Fixed query tuple'

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-lists all states from the database hbtn_0e_0_usa
+takes in an argument and displays all values in the states
+itable of hbtn_0e_0_usa where name matches the argument.
 """
 import sys
 import MySQLdb
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(query, (sys.argv[4]))
+    cursor.execute(query, (sys.argv[4],))
     states = cursor.fetchall()
 
     for state in states:
