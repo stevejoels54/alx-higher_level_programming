89075f8e40533dba967e117045615744d4a18520
joelsteven <joelofelectronics@gmail.com>
2023-09-18 01:57:10 +0300

    Fixed format query string

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,10 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(query, (sys.argv[4],))
+    query = ("SELECT * FROM states "
+             "WHERE name = '{}' "
+             "ORDER BY id".format(sys.argv[4]))
+    cursor.execute(query)
     states = cursor.fetchall()
 
     for state in states:
