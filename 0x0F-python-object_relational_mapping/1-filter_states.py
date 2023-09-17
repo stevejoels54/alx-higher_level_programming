5b5c28dcc41a77725d903c9e9490bab3b81454d4
joelsteven <joelofelectronics@gmail.com>
2023-09-18 02:09:46 +0300

    Fixed case sensitivity on queries

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cursor.execute("SELECT * FROM states WHERE name"
+                   "LIKE BINARY 'N%' ORDER BY id")
     states = cursor.fetchall()
 
     for state in states:
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = ("SELECT * FROM states "
-             "WHERE name = '{}' ".format(sys.argv[4]))
+             "WHERE BINARY name = '{}' ".format(sys.argv[4]))
     cursor.execute(query)
     states = cursor.fetchall()
 
