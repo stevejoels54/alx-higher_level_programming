9c0085ca4e720a3dad0593707c971a2345b061ea
joelsteven <joelofelectronics@gmail.com>
2023-09-18 02:01:49 +0300

    Fixed order by

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = ("SELECT * FROM states "
-             "WHERE name = '{}' "
-             "ORDER BY id".format(sys.argv[4]))
+             "WHERE name = '{}' ".format(sys.argv[4]))
     cursor.execute(query)
     states = cursor.fetchall()
 
