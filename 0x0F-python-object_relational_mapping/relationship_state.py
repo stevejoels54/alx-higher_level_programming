f62b34fbeb84bc36ae5a3755f39538a763afd546
joelsteven <joelofelectronics@gmail.com>
2023-09-18 05:27:31 +0300

    Fixed relations

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,8 +5,8 @@ class definition of a State and an instance Base = declarative_base()
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_city import Base, City
+from sqlalchemy.orm import relationship
 
 
 class State(Base):
@@ -14,5 +14,4 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan",
-                          back_populates="state")
+    cities = relationship("City", backref="state", cascade="all, delete")
