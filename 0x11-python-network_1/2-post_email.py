72ab353c7c5b51a0e44e735b3d5ca0548a816edf
joelsteven <joelofelectronics@gmail.com>
2023-09-30 05:29:34 +0300

    Fixed output

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,12 +9,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
 
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        body = response.read()
-        utf8_body = body.decode('utf-8')
-
-    print("Your email is: {}".format(email))
-    print(utf8_body)
+        print(response.read().decode("utf-8"))
