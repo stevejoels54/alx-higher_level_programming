9a141a00104cc9ae8b83748d3af8652b38abf9da
joelsteven <joelofelectronics@gmail.com>
2023-10-25 04:33:16 +0300

    Added task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+fs.writeFile(filePath, stringToWrite, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
