5269f611b8f3cf26944bffa5a0b779c5d88bc127
joelsteven <joelofelectronics@gmail.com>
2023-10-25 05:16:08 +0300

    Added task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const request = require('request');
+const url = process.argv[2];
+request(url, (error, response, body) => {
+  if (!error) {
+    const filmsData = JSON.parse(body);
+    const movies = filmsData.results.filter((film) => {
+        return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+    });
+    console.log(movies.length);
+  }
+});
