636f1e9efe04680afed1e33f13d2235497aa3969
joelsteven <joelofelectronics@gmail.com>
2023-10-25 05:17:42 +0300

    Fixed semistandard style and shellbang

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
   if (!error) {
     const filmsData = JSON.parse(body);
     const movies = filmsData.results.filter((film) => {
-        return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+      return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
     });
     console.log(movies.length);
   }
